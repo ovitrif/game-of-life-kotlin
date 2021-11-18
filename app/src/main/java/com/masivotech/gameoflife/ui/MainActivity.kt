0d6e8535e6eea8712805db1256e50b91299a3bd9
Ovi Trif <otr@bringme.com>
2021-11-18 20:34:03 +0100

    Use view binding

--- app/src/main/java/com/masivotech/gameoflife/ui/MainActivity.kt
+++ app/src/main/java/com/masivotech/gameoflife/ui/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.masivotech.gameoflife.App
 import com.masivotech.gameoflife.R
+import com.masivotech.gameoflife.databinding.ActivityMainBinding
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +13,13 @@ class MainActivity : AppCompatActivity() {
     @Inject
     lateinit var viewModelFactory: ViewModelProvider.Factory
 
+    private lateinit var binding: ActivityMainBinding
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         App.INSTANCE.container.inject(this)
         viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
