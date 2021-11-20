5eee8c6d8fbad0f8caa166889e221c8e2d18ddfc
Ovi Trif <otr@bringme.com>
2021-11-20 20:23:38 +0100

    TDD get cell neighbors

--- app/src/main/java/com/masivotech/gameoflife/ui/domain/Game.kt
+++ app/src/main/java/com/masivotech/gameoflife/ui/domain/Game.kt
@@ -1,6 +1,5 @@
 package com.masivotech.gameoflife.ui.domain
 
-import java.lang.IllegalStateException
 import kotlin.random.Random
 
 
@@ -15,10 +14,11 @@ data class Cell(
     val y: Int,
 ) {
     override fun toString(): String {
-        return when (state) {
+        val state = when (state) {
             CellState.LIVE -> "🟩"
             CellState.DEAD -> "🟥"
         }
+        return "[x=$x, y=$y, $state]"
     }
 }
 
@@ -54,7 +54,13 @@ class Game {
     }
 
     fun runGameRound() {
-        TODO()
+        for (x in 0 until board.width) {
+            for (y in 0 until board.height) {
+                var cell = board.cells.find { cell -> cell.x == x && cell.y == y }
+                    ?: throw NullPointerException("Couldn't find cell on position [ x=$x y=$y ]")
+                cell = calculateNextCellState(cell)
+            }
+        }
     }
 
     fun calculateNextCellState(cell: Cell): Cell {
@@ -62,12 +68,30 @@ class Game {
     }
 
     fun getCellNeighbors(cell: Cell): ArrayList<Cell> {
-        TODO()
+        val neighbors = ArrayList<Cell>()
+
+        for (x in -1..1) {
+            for (y in -1..1) {
+                val neighborX = cell.x + x
+                val neighborY = cell.y + y
+                val neighborCell = getCell(neighborX, neighborY)
+
+                if (neighborCell != null && neighborCell != cell) {
+                    neighbors.add(neighborCell)
+                }
+            }
+        }
+
+        return neighbors
     }
 
     private fun createRandomCellState(): CellState {
         val random = Random.nextInt(0, 2)
         return CellState.values()[random]
+    }
+
+    fun getCell(x: Int, y: Int): Cell? {
+        return board.cells.find { it.x == x && it.y == y }
     }
 
     fun printBoard(): String {
--- app/src/test/java/com/masivotech/gameoflife/ui/domain/GameTest.kt
+++ app/src/test/java/com/masivotech/gameoflife/ui/domain/GameTest.kt
@@ -15,10 +15,61 @@ class GameTest {
 
     @Test
     fun `verify board size`() {
-        game.seed()
-
         println(game.printBoard())
 
         assertThat(game.board.cells).hasSize(game.board.width * game.board.height)
+    }
+
+    @Test
+    fun `verify number of neighbors is 3 for cells at corners`() {
+        val cell1 = game.getCell(0, 0)!!
+        val neighborsCell1 = game.getCellNeighbors(cell1)
+        val cell2 = game.getCell(game.board.width - 1, 0)!!
+        val neighborsCell2 = game.getCellNeighbors(cell2)
+        val cell3 = game.getCell(0, game.board.height - 1)!!
+        val neighborsCell3 = game.getCellNeighbors(cell3)
+        val cell4 = game.getCell(game.board.width - 1, game.board.height - 1)!!
+        val neighborsCell4 = game.getCellNeighbors(cell4)
+
+        assertThat(neighborsCell1).hasSize(3)
+        assertThat(neighborsCell2).hasSize(3)
+        assertThat(neighborsCell3).hasSize(3)
+        assertThat(neighborsCell4).hasSize(3)
+    }
+
+
+    @Test
+    fun `verify number of neighbors is 5 for cells at edges excluding corners`() {
+        // verify cells on first row
+        for (x in 1 until game.board.width - 1) {
+            val cell = game.getCell(x, 0)!!
+            val neighbors = game.getCellNeighbors(cell)
+
+            assertThat(neighbors).hasSize(5)
+        }
+
+        // verify cells on first column
+        for (y in 1 until game.board.height - 1) {
+            val cell = game.getCell(0, y)!!
+            val neighbors = game.getCellNeighbors(cell)
+
+            assertThat(neighbors).hasSize(5)
+        }
+
+        // verify cells on last row
+        for (x in 1 until game.board.width - 1) {
+            val cell = game.getCell(x, game.board.height - 1)!!
+            val neighbors = game.getCellNeighbors(cell)
+
+            assertThat(neighbors).hasSize(5)
+        }
+
+        // verify cells on last column
+        for (y in 1 until game.board.height - 1) {
+            val cell = game.getCell(game.board.width - 1, y)!!
+            val neighbors = game.getCellNeighbors(cell)
+
+            assertThat(neighbors).hasSize(5)
+        }
     }
 }
