4dc06971b6c38c695c2b7433b97cdf9f8fddf8ff
Ovi Trif <otr@bringme.com>
2021-11-21 18:59:28 +0100

    Delete unused code

--- app/src/main/java/com/masivotech/gameoflife/core/extensions/RxExtensions.kt
+++
@@ -1,9 +0,0 @@
-package com.masivotech.gameoflife.core.extensions
-
-import io.reactivex.Single
-import io.reactivex.disposables.Disposable
-
-/**
- * Subscribes with empty consumers.
- */
-fun <T : Any> Single<T>.consume(): Disposable = subscribe({}, {})
--- app/src/test/java/com/masivotech/gameoflife/ui/MainViewModelTest.kt
+++
@@ -1,8 +0,0 @@
-package com.masivotech.gameoflife.ui
-
-import org.junit.runner.RunWith
-import org.mockito.junit.MockitoJUnitRunner
-
-@RunWith(MockitoJUnitRunner::class)
-class MainViewModelTest {
-}
